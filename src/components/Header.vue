59849efb3de7237af7a5dffa98cb8e9a14c24ed8
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-09-27 15:01:56 +0700

    basic component selesai

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+const text = 'Hello World'
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: aquamarine;
+  font-weight: bolder;
+}
+</style>
